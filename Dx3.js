f35cf85fd8225d3d60d29170037948b4ac04d960
zeteticl <cougar.l@hotmail.com>
2018-03-13 12:45:27 +0800

    DX3

---
+++ Dx3.js
@@ -0,0 +1,80 @@
+on("chat:message", function(msg) {
+  //This allows players to enter !sr <number> to roll a number of d6 dice with a target of 4.
+  if(msg.type == "api" && msg.content.indexOf("!sad ") !== -1) {
+      try{
+    var numdice = msg.content.replace("!sad ", "");
+    sendChat(msg.who, dx(numdice));
+  }
+  catch(err)
+  {
+  //ERROR, So  Sad QAQ
+  }
+  }
+});
+
+
+function dx(triggermsg) {
+	//var varcou = 0;
+	var finallynum = 0;
+	var returnStr = triggermsg + ' → ';
+	var match = /^(\d+)(dx)(\d|)(((\+|-)(\d+)|)((\+|-)(\d+)|))$/i.exec(triggermsg);	//判斷式  [0]2dx8-2+10,[1]2,[2]dx,[3]8,[4]-2+10,[5]-2,[6]-,[7]2,[8]+10,[9]+,[10]10  
+//	console.log(match);
+	if (match[3] == "") { match[3] = 10 }
+	if (match[3] <= 2) {
+		var rply = '加骰最少比2高';
+		return rply;
+	}
+
+	for (var round = 1; round > 0; round--) {
+		[match, round, returnStr, finallynum] = dxroll(match, round, returnStr, finallynum);
+	}
+	returnStr = returnStr.replace(/[,][ ]+]/ig, ']');
+	if (match[6] == '+') {
+		for (var i = 0; i < Number(match[7]); i++) {
+			finallynum++;
+		}
+	}
+	if (match[6] == '-') {
+		for (var i = 0; i < Number(match[7]); i++) {
+			finallynum--;
+		}
+	}
+	if (match[9] == '+') {
+		for (var i = 0; i < Number(match[10]); i++) {
+			finallynum++;
+		}
+	}
+	if (match[9] == '-') {
+		for (var i = 0; i < Number(match[10]); i++) {
+			finallynum--;
+		}
+	}
+	returnStr += match[4] + ' → ' + finallynum;
+	var rply = returnStr;
+	return rply;
+
+}
+
+function dxroll(match, round, returnStr, finallynum) {
+	var result = 0;
+	var rollnum = match[1];
+	match[1] = 0;
+	var varcou = "";
+	var varsu = "";
+	for (var i = 0; i < rollnum; i++) {
+		varcou = Math.floor(Math.random() * 10) + 1;
+		if (varcou > result) { result = varcou }
+		if (varcou >= Number(match[3])) {
+			result = 10;
+			match[1]++;
+		}
+		varsu += varcou + ', ';
+	}
+	returnStr += result + '[' + varsu + '] ';
+	finallynum += Number(result);
+	if (match[1] >= 1) {
+		round++;
+		returnStr += '+ ';
+	}
+	return [match, round, returnStr, finallynum];
+}
